f8f8f410a8dd841734cfdc54e47232b2e6475b11
DanielSedell02 <Danne.sedell@gmail.com>
2024-04-16 19:51:26 +0200

    fixed tailwind problem

--- React-Weather-App/src/App.tsx
+++ React-Weather-App/src/App.tsx
@@ -1,8 +1,10 @@
 export default function App() {
   return (
-    <h1 className="text-4xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <main
+      className="flex justify-center items-center bg-gradient-to-r from-cyan-500 to-blue-500
+    h-[100vh] w-full"
+    >
+      <section className="bg-white bg-opacity-20">Hello world!</section>
+    </main>
+  );
 }
-
