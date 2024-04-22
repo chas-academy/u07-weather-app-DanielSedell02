a8204c44744bcd78a6bb405480d9cbf482afdc90
DanielSedell02 <Danne.sedell@gmail.com>
2024-04-22 10:51:20 +0200

    split the code into the Search component

--- React-Weather-App/src/App.tsx
+++ React-Weather-App/src/App.tsx
@@ -1,10 +1,12 @@
 import { ChangeEvent, useEffect, useState } from "react";
 import { optionType } from "./types";
+import Search from "./components/Search";
 
 const App = (): JSX.Element => {
   const [term, setTerm] = useState<string>("");
   const [city, setCity] = useState<optionType | null>(null);
   const [options, setOptions] = useState<[]>([]);
+  const [Forecast, setForecast] = useState<forecastType | null>(null);
 
   const getSearchOptions = (value: string) => {
     const apiKey = import.meta.env.VITE_API_KEY;
@@ -36,7 +38,7 @@ const App = (): JSX.Element => {
 
     fetch(apiUrl)
       .then((response) => response.json())
-      .then((data) => console.log({ data }))
+      .then((data) => setForecast(data))
 
       // Handle the response data
 
@@ -71,42 +73,17 @@ const App = (): JSX.Element => {
       className="flex justify-center items-center bg-gradient-to-r from-cyan-500 to-blue-500
     h-[100vh] w-full"
     >
-      <section
-        className="w-full md:max-w-[500px] p-4 flex flex-col 
-      text center items-center justify-center md:px-10 lg:p-24 
-      h-full lg:h-[500px] bg-white bg-opacity-40"
-      >
-        <h1 className="text-3xl">Weather Forecast</h1>
-        <div className="relative flex items-center">
-          <input
-            type="text"
-            value={term}
-            className="flex-1 px-3 py-2 rounded-l-md border border-white focus:outline-none"
-            onChange={onInputChange}
-          />
-
-          <ul className=" absolute top-9 bg-white m1-1 rounded-b-md">
-            {options.map((option: optionType, index: number) => (
-              <li key={option.name + "-" + index}>
-                <button
-                  className="text-left text-sm w-full hover:bg-sky-700 
-                hover:text-white px-3 py-1 cursor-pointer"
-                  onClick={() => onOptionSelect(option)}
-                >
-                  {option.name}
-                </button>
-              </li>
-            ))}
-          </ul>
-
-          <button
-            className="px-4 py-2 rounded-r-md border-2 border-white ml-2"
-            onClick={onSubmit}
-          >
-            Search
-          </button>
-        </div>
-      </section>
+      {Forecast ? (
+        "we have a forecast"
+      ) : (
+        <Search
+          term={term}
+          options={options}
+          onInputChange={onInputChange}
+          onOptionSelect={onOptionSelect}
+          onSubmit={onSubmit}
+        />
+      )}
     </main>
   );
 };
