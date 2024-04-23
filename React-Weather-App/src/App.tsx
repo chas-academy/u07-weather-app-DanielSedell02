f1300ca573dfa06d4190b35fa21520f365621252
DanielSedell02 <Danne.sedell@gmail.com>
2024-04-23 09:26:29 +0200

    fixed some small errors

--- React-Weather-App/src/App.tsx
+++ React-Weather-App/src/App.tsx
@@ -1,12 +1,15 @@
 import Search from "./components/Search";
+import useForecast from "./hooks/useForecast";
 
 const App = (): JSX.Element => {
+  const { term, options, forecast, onInputChange, onOptionSelect, onSubmit } =
+    useForecast();
   return (
     <main
       className="flex justify-center items-center bg-gradient-to-r from-cyan-500 to-blue-500
     h-[100vh] w-full"
     >
-      {Forecast ? (
+      {forecast ? (
         "we have a forecast"
       ) : (
         <Search
--- React-Weather-App/src/hooks/useForecast.ts
+++ React-Weather-App/src/hooks/useForecast.ts
@@ -76,6 +76,7 @@ const useForecast = () => {
     forecast,
     onInputChange,
     onOptionSelect,
+    onSubmit,
   };
 };
 
