7ed30759759a19e5e6030307edb5002e902414be
DanielSedell02 <Danne.sedell@gmail.com>
2024-04-24 23:32:04 +0200

    created a new component

---
+++ React-Weather-App/src/components/TileCardComponent.tsx
@@ -0,0 +1,20 @@
+type Props = {
+    title: string
+    info: string | JSX.Element
+    description: string
+
+}
+
+const  = {
+    wind: 
+}
+
+const TileCardComponent = ({ title, info, description }: Props): JSX.Element => {
+  return (
+    <div>
+      <p>Tile</p>
+    </div>
+  );
+};
+
+export default TileCardComponent;
