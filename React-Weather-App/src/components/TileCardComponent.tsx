818ea4cb39876ab64a640fbe46ccdc7a99c77823
DanielSedell02 <Danne.sedell@gmail.com>
2024-04-27 01:10:18 +0200

    little styling

--- React-Weather-App/src/components/TileCardComponent.tsx
+++ React-Weather-App/src/components/TileCardComponent.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const TileCardComponent = ({ title, info }: Props): JSX.Element => {
   return (
-    <article className="w-[110px] h-[120px] bg-slate-400 backdrop-ls rounded-lg shadow-md font-bold mr-4 mb-4">
+    <article className="w-[110px] h-[120px] bg-gray-700 backdrop-ls rounded-lg shadow-md font-bold mr-4 mb-4">
       <div className="flex items-center space-x-2">
         <h3>{title}</h3>
       </div>
