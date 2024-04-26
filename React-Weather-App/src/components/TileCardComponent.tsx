2f97a4ebb11f9a1e7613bb68dc355b273a2cc217
DanielSedell02 <Danne.sedell@gmail.com>
2024-04-26 20:28:12 +0200

    removed Icons

--- React-Weather-App/src/components/TileCardComponent.tsx
+++ React-Weather-App/src/components/TileCardComponent.tsx
@@ -1,26 +1,16 @@
-import Wind from "./Icons/Wind";
+import { JSX } from "react";
 
 type Props = {
-  icon: "wind" | "feels" | "humidity" | "visibility";
   title: string;
   info: string | JSX.Element;
   description: string;
 };
 
-const icons = {
-  wind: Wind,
-  // feels: Feels,
-  // humidity: Humidity,
-  // visibility: visibility,
-};
-
-const TileCardComponent = ({ icon, title, info }: Props): JSX.Element => {
-  const Icon = icons[icon];
-
+const TileCardComponent = ({ title, info }: Props): JSX.Element => {
   return (
-    <article className="w-[140px] h-[130px] bg-slate-400 backdrop-ls">
+    <article className="w-[110px] h-[120px] bg-slate-400 backdrop-ls rounded-lg shadow-md font-bold mr-4 mb-4">
       <div className="flex items-center space-x-2">
-        <Icon /> <h3>{title}</h3>
+        <h3>{title}</h3>
       </div>
       <h4>{info}</h4>
     </article>
