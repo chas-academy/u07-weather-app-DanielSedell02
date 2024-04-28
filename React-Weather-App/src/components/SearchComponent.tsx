75c1b9d3fe0b14da64fc51d677e61ffe2b4bbd87
DanielSedell02 <Danne.sedell@gmail.com>
2024-04-28 13:09:15 +0200

    GeoLocation added

--- React-Weather-App/src/components/SearchComponent.tsx
+++ React-Weather-App/src/components/SearchComponent.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import { optionType } from "../types";
 
 type Props = {
@@ -16,16 +16,32 @@ const SearchComponent = ({
   onOptionSelect,
   onSubmit,
 }: Props): JSX.Element => {
+  const [coordinates, setCoordinates] = useState<{
+    lat: number;
+    long: number;
+  } | null>(null);
+
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setCoordinates({
+            lat: position.coords.latitude,
+            long: position.coords.longitude,
+          });
+        },
+        (error) => {
+          console.error("Error getting user location:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  };
+
   return (
-    <main
-      className="flex justify-center items-center bg-gradient-to-r from-cyan-500 to-blue-500
-    h-[100vh] w-full"
-    >
-      <section
-        className="w-full md:max-w-[500px] p-4 flex flex-col 
-      text center items-center justify-center md:px-10 lg:p-24 
-      h-full lg:h-[500px] bg-white bg-opacity-40"
-      >
+    <main className="flex justify-center items-center bg-gradient-to-r from-cyan-500 to-blue-500 h-[100vh] w-full">
+      <section className="w-full md:max-w-[500px] p-4 flex flex-col text center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-40">
         <h1 className="text-3xl">Weather Forecast</h1>
         <div className="relative flex items-center">
           <input
@@ -34,13 +50,11 @@ const SearchComponent = ({
             className="flex-1 px-3 py-2 rounded-l-md border border-white focus:outline-none"
             onChange={onInputChange}
           />
-
-          <ul className=" absolute top-9 bg-white m1-1 rounded-b-md">
+          <ul className="absolute top-9 bg-white m1-1 rounded-b-md">
             {options.map((option: optionType, index: number) => (
               <li key={option.name + "-" + index}>
                 <button
-                  className="text-left text-sm w-full hover:bg-sky-700 
-                hover:text-white px-3 py-1 cursor-pointer"
+                  className="text-left text-sm w-full hover:bg-sky-700 hover:text-white px-3 py-1 cursor-pointer"
                   onClick={() => onOptionSelect(option)}
                 >
                   {option.name}
@@ -48,14 +62,25 @@ const SearchComponent = ({
               </li>
             ))}
           </ul>
-
           <button
             className="px-4 py-2 rounded-r-md border-2 border-white ml-2"
             onClick={onSubmit}
           >
             Search
           </button>
+          <button
+            className="px-4 py-2 rounded-md border-2 border-white ml-2"
+            onClick={getLocation}
+          >
+            Get Location
+          </button>
         </div>
+        {coordinates && (
+          <div className="mt-4">
+            <p>Latitude: {coordinates.lat}</p>
+            <p>Longitude: {coordinates.long}</p>
+          </div>
+        )}
       </section>
     </main>
   );
