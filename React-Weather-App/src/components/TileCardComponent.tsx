dc7fccf5b0eb6d103c7171dabbac975079d137c9
DanielSedell02 <Danne.sedell@gmail.com>
2024-08-13 00:58:28 +0200

    TileCard center text

--- React-Weather-App/src/components/TileCardComponent.tsx
+++ React-Weather-App/src/components/TileCardComponent.tsx
@@ -7,9 +7,9 @@ type Props = {
 
 const TileCardComponent = ({ title, info }: Props): JSX.Element => {
   return (
-    <article className="w-[110px] h-[120px] bg-gray-700 backdrop-ls rounded-lg shadow-md font-bold mr-4 mb-4">
-      <div className="flex items-center text-center space-x-2">
-        <h3>{title}</h3>
+    <article className="w-[120px] h-[120px] bg-light-blue-700 backdrop-ls rounded-lg shadow-md font-bold mr-5 mb-6 flex justify-center items-center">
+      <div className="flex items-center text-center justify-center ">
+        <h3 className=" mr-2">{title}</h3>
       </div>
       <h4>{info}</h4>
     </article>
